ab0c915ffd19a5c896e6286c922a8f22cfe991c0
mattdotrs <ysmatiasivrgs@gmail.com>
2026-01-22 15:51:39 -0600

    Added functionality for starting and resetting timers, notification plays when timer stops

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
 import { getCurrentWindow } from '@tauri-apps/api/window';
+import {
+  isPermissionGranted,
+  requestPermission,
+  sendNotification,
+} from '@tauri-apps/plugin-notification';
 
 // when using `"withGlobalTauri": true`, you may use
 // const { getCurrentWindow } = window.__TAURI__.window;
+let permissionGranted = await isPermissionGranted();
+
+// If not we need to request it
+if (!permissionGranted) {
+  const permission = await requestPermission();
+  permissionGranted = permission === 'granted';
+}
 
 const appWindow = getCurrentWindow();
 
@@ -24,15 +36,39 @@ interface Time {
 let focusSession = 1500;
 let breakTime = 300;
 let currentTime = 0;
+let lastUsedTime = 0;
+let interval : any;
 
-function updateTimer(time : number) : any {
-    currentTime = time
+function updateTimer(time? : number) : any {
+    if (time !== undefined) {currentTime = time;}
     let currentSession: Time = {
         minutes: Math.floor(currentTime / 60),
         seconds: currentTime % 60
     };
-    
+    if (currentTime <= 60) {
+        timerSpan.style["color"] = "var(--accent)";
+    } else {
+        timerSpan.style["color"] = "var(--textMain)";
+    }
     timerSpan.innerText = `${currentSession.minutes}:${currentSession.seconds.toString().padStart(2, '0')}`;
+}
+
+function startTimer() {
+    lastUsedTime = currentTime;
+    interval = setInterval(() => {
+        --currentTime;
+        updateTimer();
+        if (currentTime === 0) {
+            clearInterval(interval);
+            var audio = new Audio('/src-tauri/audio/finish.wav');
+            audio.play();
+            if (permissionGranted) {
+                sendNotification({ title: 'Pomodoro', body: 'Time\'s up!' });
+            }
+            currentTime = lastUsedTime;
+            updateTimer();
+        }
+    }, 1000)
 }
 
 updateTimer(focusSession);
@@ -49,3 +85,21 @@ document.getElementById('BreakSelect')?.addEventListener('click', () => {
     updateTimer(breakTime);
 });
 
+document.getElementById('StartTimer')?.addEventListener('click', () => {
+    startTimer();
+    document.getElementById('StartTimer').style["display"] = "none";
+    document.getElementById('StopTimer').style["display"] = "inline";
+});
+
+document.getElementById('StopTimer')?.addEventListener('click', () => {
+    clearInterval(interval);
+    document.getElementById('StartTimer').style["display"] = "inline";
+    document.getElementById('StopTimer').style["display"] = "none";
+});
+
+document.getElementById('ResetTimer')?.addEventListener('click', () => {
+    clearInterval(interval);
+    clearInterval(interval);
+    currentTime = lastUsedTime;
+    updateTimer();
+});
